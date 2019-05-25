5d248b2aeba6bcdc01482bc666172af7516b45a5
Jack Lee <linb.net@gmail.com>
2019-05-25 14:03:28 -0400

    Updated by CrossUI Web App Builder

--- Module/js/GithubAPI.js
+++ Module/js/GithubAPI.js
@@ -32,7 +32,7 @@ xui.Class('Module.Module_Githubapi1', 'xui.Module',{
                     .setShowEffects("Slide In TB")
                     .setHideEffects("Slide In TB")
                     .setPanelBgClr("#fff")
-                    .setIframeAutoLoad("{/}Module/js/Module.Module_Githubapi1/pages/GithubLoginLayer.html");
+                    .setIframeAutoLoad("{/}Module/js/Module.GithubAPI/pages/GithubLoginLayer.html");
                 // ensure effects
                 loginLayer.render(true);
             }
