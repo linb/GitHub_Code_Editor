e056f5ca1e15b4da1fca594f59ac6dfdc65548bd
Jack Lee <linb.net@gmail.com>
2019-05-25 22:09:35 -0400

    Updated by CrossUI Web App Builder

--- Module/js/CodeMirror5.js
+++ Module/js/CodeMirror5.js
@@ -31,8 +31,7 @@ xui.Class('Module.CodeMirror5', 'xui.Module',{
         propSetAction : function(prop){
         },
         attachCodeMirror : function(div, path, value){
-            var textarea = inputCtrl.getSubNode("INPUT").get(0),
-                ext = path.split(".").pop(),
+            var ext = path.split(".").pop(),
                 mode = CodeMirror.findModeByExtension(ext) ||  CodeMirror.findModeByExtension("txt");
             var cm = new CodeMirror(function(elt){
                 div.getRoot().append(elt);
